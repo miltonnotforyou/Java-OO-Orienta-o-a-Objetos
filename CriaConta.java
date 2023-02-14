0686a072b11acd1f860697a95705f03abff21657
Miltonnnotforyou <67373586+miltonnotforyou@users.noreply.github.com>
2023-02-14 09:02:16 -0300

    Add files via upload

---
+++ CriaConta.java
@@ -0,0 +1,20 @@
+package bytebank;
+
+public class CriaConta {
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta);
+		
+		primeiraConta.saldo += 100;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("Primeira conta tem " + primeiraConta.saldo);
+		System.out.println("Primeira conta tem " + segundaConta.saldo);
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author milton
+ *
+ */
+module bytebank {
+}
